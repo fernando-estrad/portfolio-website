b0ddec80267ecf8d067b4c5744052038ff55b97d
Fernando Estrada <estradam.fernando@gmail.com>
2020-03-31 16:17:30 -0600

    Updates

---
+++ precache-manifest.0422ce6f8603ab1e4cd11598e3cefe1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d52ec1abe852310b65cb48a056ce70b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e79ec37d266c8ab631ec",
+    "url": "/static/css/main.8dba1303.chunk.css"
+  },
+  {
+    "revision": "fefbd40707a45dce46c9",
+    "url": "/static/js/2.66b44795.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b44795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79ec37d266c8ab631ec",
+    "url": "/static/js/main.57a134da.chunk.js"
+  },
+  {
+    "revision": "8cdf164702af695058ba",
+    "url": "/static/js/runtime-main.223301a1.js"
+  },
+  {
+    "revision": "34a88ad1489eb7de83ffd8142b80bf8a",
+    "url": "/static/media/Fernando-Estrada-Resume.34a88ad1.pdf"
+  },
+  {
+    "revision": "0e0b002ecdd030fc0594c0d7d6e70fcf",
+    "url": "/static/media/bg-img.0e0b002e.jpg"
+  },
+  {
+    "revision": "33544c46acb574de066284a5a65ed1a3",
+    "url": "/static/media/profile.33544c46.JPG"
+  }
+]);
