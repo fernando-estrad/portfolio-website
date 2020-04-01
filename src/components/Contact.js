bfdbabd359b0bc5728d3de7305a49b8d6e197abf
Fernando Estrada <estradam.fernando@gmail.com>
2020-03-31 19:21:49 -0600

    fixed ?

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Contact(){
+    return (
+        <div className="contact" id="contact">
+            <h3>Contact Me</h3>
+            <p><i class="fas fa-envelope" style={{fontSize: "20px"}}></i> <a href="mailto:estradam.fernando@gmail.com">estradam.fernando@gmail.com</a></p>
+            <br />
+            <p><i class="fas fa-mobile-alt" style={{fontSize: "20px"}}></i> <a href="tel:3074134272">307-413-4272</a></p>
+        </div>
+    )
+}
