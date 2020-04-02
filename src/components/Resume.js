d4b522a049cde96e4a4ab1a4922babb5d69f2a9e
Fernando Estrada <estradam.fernando@gmail.com>
2020-04-02 15:02:05 -0600

    Changed resume to download.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,10 @@
 import React from "react"
-import bgIMG from "../images/Fernando-Estrada-Resume.pdf"
+import resume from "../images/Fernando_Estrada_Resume.pdf"
 
 export default function Resume(){
     return (
         <div className="download-resume">
-            <a href={bgIMG} download>Download Resume <i class="fas fa-download"></i></a>
+            <a href={resume} download>Download Resume <i class="fas fa-download"></i></a>
         </div>
         
     )
